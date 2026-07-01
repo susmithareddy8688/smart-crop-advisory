f8ff88baeca85e1152341704383a5520e7f2ff62
susmithareddy8688 <chinnireddypasala@gmail.com>
2026-07-01 11:41:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 CORS(app)
 
 # ==========================================
